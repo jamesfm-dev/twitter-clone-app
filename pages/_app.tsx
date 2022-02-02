6143d9d000c3bebfecccd3d3c80ab23367a19985
jamesfm-dev <66410095+jamesfm-dev@users.noreply.github.com>
2022-02-02 19:24:19 +0800

    build: add app title meta viewport & main tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Twitter Clone</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <main>
+        <Component {...pageProps} />;
+      </main>
+    </>
+  );
 }
 
 export default MyApp;
