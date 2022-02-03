32c456b98adc6af9fa879ca1d12b90f27c90b965
jamesfm-dev <66410095+jamesfm-dev@users.noreply.github.com>
2022-02-03 19:04:27 +0800

    chore: change maxWidth to lg on container & add prop types

--- components/BaseContainer.tsx
+++ components/BaseContainer.tsx
@@ -1,13 +1,17 @@
 import Container, { ContainerProps } from "@mui/material/Container";
 
-const BaseContainer = ({ children }: ContainerProps) => {
+export interface BaseContainerProps extends ContainerProps {
+  children: React.ReactNode;
+}
+
+const BaseContainer = ({ children }: BaseContainerProps) => {
   return (
     <Container
       disableGutters={true}
       maxWidth={false}
       sx={{ background: "black", minHeight: "100vh" }}
     >
-      <Container disableGutters={false} maxWidth="xl">
+      <Container disableGutters={false} maxWidth="lg">
         {children}
       </Container>
     </Container>
